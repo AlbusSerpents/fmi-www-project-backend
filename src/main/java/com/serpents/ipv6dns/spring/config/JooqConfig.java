e4085ac4f2168e796ce7e830a73371bd9bc2e80a
D.Naydenov <d.naydenov@futurist-labs.com>
2018-05-27 19:49:41 +0300

    Configured JOOQ's transaction support with spring's @Transaction aspect.

--- src/main/java/com/serpents/ipv6dns/spring/config/JooqConfig.java
+++ src/main/java/com/serpents/ipv6dns/spring/config/JooqConfig.java
@@ -4,14 +4,21 @@ package com.serpents.ipv6dns.spring.config;
 import com.serpents.ipv6dns.spring.properties.ApplicationProperties;
 import com.serpents.ipv6dns.spring.properties.ApplicationPropertiesKey;
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.jooq.ConnectionProvider;
 import org.jooq.DSLContext;
+import org.jooq.impl.DataSourceConnectionProvider;
+import org.jooq.impl.DefaultConfiguration;
+import org.jooq.impl.DefaultExecuteListenerProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.sql.DataSource;
-import java.sql.SQLException;
 
 import static org.jooq.SQLDialect.POSTGRES;
 import static org.jooq.impl.DSL.using;
@@ -27,20 +34,37 @@ public class JooqConfig {
         databaseUrl = applicationProperties.getProperty(ApplicationPropertiesKey.JDBC_URL);
     }
 
-    @Bean(destroyMethod = "close")
+    @Bean
     public DataSource dataSource() {
         final BasicDataSource dataSource = new BasicDataSource();
         dataSource.setUrl(databaseUrl);
-        return dataSource;
+
+        return new TransactionAwareDataSourceProxy(dataSource);
     }
 
     @Bean
-    public DSLContext dslContext(final DataSource dataSource) {
-        try {
-            return using(dataSource.getConnection(), POSTGRES);
-        } catch (SQLException sqle) {
-            System.err.println("Couldn't initialize connection");
-            throw new RuntimeException(sqle);
-        }
+    @Primary
+    public ConnectionProvider connectionProvider(final DataSource dataSource) {
+        return new DataSourceConnectionProvider(dataSource);
+    }
+
+    @Bean
+    public org.jooq.Configuration configuration(final DataSource dataSource, final ConnectionProvider provider, final DefaultExecuteListenerProvider executeListenerProvider) {
+        return new DefaultConfiguration()
+                .derive(POSTGRES)
+                .derive(provider)
+                .derive(executeListenerProvider);
+    }
+
+
+    @Bean
+    public DSLContext dslContext(final org.jooq.Configuration configuration) {
+        return using(configuration);
+    }
+
+    @Bean(name = "transactionManager")
+    @Autowired
+    public PlatformTransactionManager getTransactionManager(final DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
     }
 }
