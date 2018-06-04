0b67a831f921e728b08bcb6facc52a4a590621b3
D.Naydenov <d.naydenov@futurist-labs.com>
2018-06-04 23:47:45 +0300

    Cleaned the gradle file, fixed the optional serialization problem and fixed the random null pointers in failed authentication.

--- src/main/java/com/serpents/ipv6dns/user/profile/ProfileUpdateRequest.java
+++ src/main/java/com/serpents/ipv6dns/user/profile/ProfileUpdateRequest.java
@@ -8,29 +8,47 @@ import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
 
-import javax.validation.constraints.NotNull;
 import java.util.Optional;
 
 import static com.fasterxml.jackson.annotation.JsonProperty.Access.WRITE_ONLY;
+import static com.serpents.ipv6dns.credentials.UserRole.fromToken;
+import static java.util.Optional.empty;
+import static java.util.Optional.ofNullable;
 
 @Data
-@NoArgsConstructor
 public class ProfileUpdateRequest {
-    @NotNull
-    private Optional<UserRole> role;
 
     @NotBlank
     @Email
     private String email;
 
+    private Optional<UserRole> role;
     private Optional<ChangePasswordRequest> passwordRequest;
+
+    public ProfileUpdateRequest() {
+        role = empty();
+        email = null;
+        passwordRequest = empty();
+    }
+
+    @JsonProperty("role")
+    public void setRole(char roleToken) {
+        this.role = fromToken(roleToken);
+    }
+
+    @JsonProperty(value = "passwordRequest")
+    public void setPasswordRequest(final ChangePasswordRequest request) {
+        this.passwordRequest = ofNullable(request);
+    }
 
     @Data
     @NoArgsConstructor
     @AllArgsConstructor
     public static class ChangePasswordRequest {
+        @NotBlank
         @JsonProperty(access = WRITE_ONLY)
         private String originalPassword;
+        @NotBlank
         @JsonProperty(access = WRITE_ONLY)
         private String newPassword;
     }
--- src/main/java/com/serpents/ipv6dns/utils/UserDetailsUtils.java
+++ src/main/java/com/serpents/ipv6dns/utils/UserDetailsUtils.java
@@ -1,7 +1,7 @@
 package com.serpents.ipv6dns.utils;
 
-import com.serpents.ipv6dns.exception.BadRequestException;
 import com.serpents.ipv6dns.spring.user.details.UserDetailsImpl;
+import org.springframework.security.authentication.BadCredentialsException;
 
 import java.util.UUID;
 
@@ -12,10 +12,12 @@ public class UserDetailsUtils {
     }
 
     public static void validateUserId(final UserDetailsImpl details, final UUID userId) {
+        if (details == null || userId == null) {
+            throw new BadCredentialsException("Unauthorized");
+        }
         final UUID detailsId = details.getUserId();
-
-        if (!detailsId.equals(userId)) {
-            throw new BadRequestException();
+        if (!userId.equals(detailsId)) {
+            throw new BadCredentialsException("Unauthorized");
         }
     }
 
