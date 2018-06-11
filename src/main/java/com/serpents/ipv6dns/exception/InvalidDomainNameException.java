e369dca0734ed305db4c2a3d0de96ab6213db998
D.Naydenov <d.naydenov@futurist-labs.com>
2018-06-12 00:39:32 +0300

    Added response InvalidDomainNameException (400)

--- src/main/java/com/serpents/ipv6dns/exception/InvalidDomainNameException.java
+++ src/main/java/com/serpents/ipv6dns/exception/InvalidDomainNameException.java
@@ -1,5 +1,10 @@
 package com.serpents.ipv6dns.exception;
 
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+@ResponseStatus(BAD_REQUEST)
 public class InvalidDomainNameException extends RuntimeException {
     public InvalidDomainNameException(final String message) {
         super(message);
