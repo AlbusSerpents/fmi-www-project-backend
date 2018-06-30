886401702bd5d40c958f1539da84de681bf2734d
D.Naydenov <naidim@abv.bg>
2018-06-30 16:46:27 +0300

    Added the CORSFilter and resolved the bug with the null pointers for unauthenticated sessions

--- src/main/java/com/serpents/ipv6dns/spring/config/SecurityConfig.java
+++ src/main/java/com/serpents/ipv6dns/spring/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.serpents.ipv6dns.spring.config;
 
+import com.serpents.ipv6dns.spring.networking.CORSFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.session.SessionManagementFilter;
 
 import static com.serpents.ipv6dns.spring.user.details.GrantedAuthorityImpl.*;
 import static org.springframework.http.HttpMethod.*;
@@ -46,51 +48,53 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin().disable()
-            .csrf().disable()
-            .exceptionHandling().authenticationEntryPoint(entryPoint)
-            .and().authorizeRequests()
+        http
+                .addFilterBefore(new CORSFilter(), SessionManagementFilter.class)
+                .formLogin().disable()
+                .csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(entryPoint)
+                .and().authorizeRequests()
 
-            // register client
-            .regexMatchers(POST, "/client").permitAll()
+                // register client
+                .regexMatchers(POST, "/client").permitAll()
 
-            // login
-            .regexMatchers(POST, "/auth").permitAll()
+                // login
+                .regexMatchers(POST, "/auth").permitAll()
 
-            // logout
-            .regexMatchers(DELETE, "/auth").hasAuthority(BASE_USER.getAuthority())
+                // logout
+                .regexMatchers(DELETE, "/auth").hasAuthority(BASE_USER.getAuthority())
 
-            // Addresses
-            .regexMatchers("addresses.*").hasAnyAuthority(ADMIN_USER.getAuthority())
+                // Addresses
+                .regexMatchers("/addresses.*").hasAnyAuthority(ADMIN_USER.getAuthority())
 
-            // Create, read or cancel request
-            .regexMatchers(POST, "request").hasAnyAuthority(CLIENT_USER.getAuthority())
-            .regexMatchers(DELETE, "request/.*").hasAnyAuthority(CLIENT_USER.getAuthority())
-            .regexMatchers(GET, "request/.*").hasAnyAuthority(CLIENT_USER.getAuthority())
+                // Create, read or cancel request
+                .regexMatchers(POST, "/request").hasAnyAuthority(CLIENT_USER.getAuthority())
+                .regexMatchers(DELETE, "/request/.*").hasAnyAuthority(CLIENT_USER.getAuthority())
+                .regexMatchers(GET, "/request/.*").hasAnyAuthority(CLIENT_USER.getAuthority())
 
-            // Read pending, reject and approve request
-            .regexMatchers(GET, "request").hasAnyAuthority(ADMIN_USER.getAuthority())
-            .regexMatchers(POST, "request/.*/approve").hasAnyAuthority(ADMIN_USER.getAuthority())
-            .regexMatchers(POST, "request/.*/reject").hasAnyAuthority(ADMIN_USER.getAuthority())
+                // Read pending, reject and approve request
+                .regexMatchers(GET, "/request").hasAnyAuthority(ADMIN_USER.getAuthority())
+                .regexMatchers(POST, "/request/.*/approve").hasAnyAuthority(ADMIN_USER.getAuthority())
+                .regexMatchers(POST, "/request/.*/reject").hasAnyAuthority(ADMIN_USER.getAuthority())
 
-            // Manage accounts
-            .regexMatchers("/manage.*").hasAuthority(ADMIN_USER.getAuthority())
+                // Manage accounts
+                .regexMatchers("/manage.*").hasAuthority(ADMIN_USER.getAuthority())
 
-            // Get domains and get by id
-            .regexMatchers(GET, "domain?.*").hasAnyAuthority(BASE_USER.getAuthority())
-            .regexMatchers(GET, "domain/view/.*").hasAnyAuthority(BASE_USER.getAuthority())
+                // Get domains and get by id
+                .regexMatchers(GET, "/domain?.*").hasAnyAuthority(BASE_USER.getAuthority())
+                .regexMatchers(GET, "/domain/view/.*").hasAnyAuthority(BASE_USER.getAuthority())
 
-            // Get all domains
-            .regexMatchers(GET, "domain/all.*").hasAnyAuthority(ADMIN_USER.getAuthority())
+                // Get all domains
+                .regexMatchers(GET, "/domain/all.*").hasAnyAuthority(ADMIN_USER.getAuthority())
 
-            // Get my domains
-            .regexMatchers(GET, "domain/my-domains/.*").hasAnyAuthority(CLIENT_USER.getAuthority())
+                // Get my domains
+                .regexMatchers(GET, "/domain/my-domains/.*").hasAnyAuthority(CLIENT_USER.getAuthority())
 
-            // user profile
-            .regexMatchers("user/.*").hasAuthority(BASE_USER.getAuthority())
+                // user profile
+                .regexMatchers("/user/.*").hasAuthority(BASE_USER.getAuthority())
 
-            // common
-            .regexMatchers(GET, "/status").permitAll();
+                // common
+                .regexMatchers(GET, "/status").permitAll();
     }
 
 
